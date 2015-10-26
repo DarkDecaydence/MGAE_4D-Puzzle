5fb1866c4f2eeca09498d2675cec776b50b9763b
Alexander Kirk Jørgensen <alkii900@msn.com>
2015-10-26 13:18:54 +0100

    Initial Project Commit

---
+++ 4D-Puzzle_Game/Assets/MouseMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var mouseX = Input.GetAxis("Mouse X");
+        var mouseY = Input.GetAxis("Mouse Y");
+
+        this.transform.Translate(new Vector3(mouseX, 0, mouseY));
+	}
+}
---
+++ 4D-Puzzle_Game/Assets/SphereCollider4D.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereCollider4D : Collider {
+    
+    public Vector3 center { get; set; }
+    public float radius { get; set; }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    
+}
