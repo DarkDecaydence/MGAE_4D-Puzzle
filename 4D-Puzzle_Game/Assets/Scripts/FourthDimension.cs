467e130376692a990f4323c50c1300b6e1f50505
Emil Buhl <ebuh@itu.dk>
2015-10-26 13:40:31 +0100

    Merging projects

---
+++ 4D-Puzzle_Game/Assets/Scripts/FourthDimension.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FourthDimension : MonoBehaviour {
+
+    private int w;
+    public int W;
+    public Material Red;
+    public Material Yellow;
+    public Material Green;
+    public Material Blue;
+    public Material Violet;
+    private int diffW;
+    private int diffWold;
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+        ChangeColor();
+        obj.layer = 8 + W;
+}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        diffW = (int)Mathf.Abs(W - PController.playerW); 
+        if (diffW != diffWold) {
+            ChangeColor();
+        }
+        diffWold = diffW;
+	}
+
+    private void ChangeColor()
+    {
+        
+        if (diffW == 0)
+        {
+            obj.GetComponent<Renderer>().material = Red;
+        }
+        else if (diffW == 1) {
+            obj.GetComponent<Renderer>().material = Yellow;
+        }
+        else if (diffW == 2)
+        {
+            obj.GetComponent<Renderer>().material = Green;
+        }
+        else if (diffW == 3)
+        {
+            obj.GetComponent<Renderer>().material = Blue;
+        }
+        else if (diffW == 4)
+        {
+            obj.GetComponent<Renderer>().material = Violet;
+        }
+    }
+}
---
+++ 4D-Puzzle_Game/Assets/Scripts/PController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PController : MonoBehaviour {
+
+    public static int playerW;
+    public float speed;
+    private float floatW;
+    private Rigidbody rb;
+    public GameObject obj;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Yaw");
+        float moveVertical = Input.GetAxis("Power");
+        float moveW = Input.GetAxis("Pitch");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        MoveFloatW(moveW*0.5f);
+        
+        /*
+        if (moveW > 0 && playerW < 4) {
+            SetW(playerW + 1);
+        } else if (moveW < 0 && playerW > 0) {
+            SetW(playerW - 1);
+        }*/
+        rb.AddForce(movement * speed);
+    }
+
+    void SetW(int w)
+    {
+        FourthDimension otherScript;
+        otherScript = GetComponent<FourthDimension>();
+        otherScript.W = w;
+        playerW = w;
+        obj.layer = 8+playerW;
+    }
+
+    void MoveFloatW(float move) {
+        floatW = floatW + move;
+        if (floatW < 0)
+        {
+            floatW = 0.0f;
+        }
+        else if (floatW > 4.9f) {
+            floatW = 4.9f;
+        }
+        SetW((int)floatW);
+    }
+
+
+}
