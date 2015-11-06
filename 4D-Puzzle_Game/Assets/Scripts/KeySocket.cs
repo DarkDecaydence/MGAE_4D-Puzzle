ddba76a90b387db526ccc3be1f3b184254cae0e1
Alexander Kirk Jørgensen <alkii900@msn.com>
2015-11-06 13:06:35 +0100

    InteractiveAdded

--- 4D-Puzzle_Game/Assets/Scripts/KeySocket.cs
+++ 4D-Puzzle_Game/Assets/Scripts/KeySocket.cs
@@ -11,7 +11,11 @@ public class KeySocket : MonoBehaviour {
     void OnTriggerStay(Collider other) {
         var potentialKey = other.gameObject.GetComponent<KeySocketable>();
 
-        if (potentialKey == null) { Debug.Log("No Key Attached!"); return; } else {
+        if (potentialKey == null) {
+            Debug.Log("No Key Attached!");
+            return;
+        }
+        else {
             var keyMatch = RequiredKey.Equals(potentialKey.Key);
 
             if (keyMatch) {
