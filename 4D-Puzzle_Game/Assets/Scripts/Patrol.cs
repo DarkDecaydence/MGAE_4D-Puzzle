97e4033b33f118bd3890e6f6ba738b96b3721f3e
Alexander Kirk Jørgensen <alkii900@msn.com>
2015-11-11 21:18:13 +0100

    Fixed minor compile warnings

--- 4D-Puzzle_Game/Assets/Scripts/Patrol.cs
+++ 4D-Puzzle_Game/Assets/Scripts/Patrol.cs
@@ -5,16 +5,11 @@ public class Patrol : FourthDimension {
     public Vector4 A;
     public Vector4 B;
     private bool goingHome = false;
-    private float fW;
     public float patrolTime;
     private float curTime = 0;
-	// Use this for initialization
-	void Start () {
-        fW = (int)W;
-	}
 	
 	// Update is called once per frame
-	void Update () {
+	protected sealed override void Update () {
         curTime = goingHome ? curTime - Time.deltaTime : curTime + Time.deltaTime;
         float lerpTime = curTime / patrolTime;
         if (curTime > patrolTime) {
