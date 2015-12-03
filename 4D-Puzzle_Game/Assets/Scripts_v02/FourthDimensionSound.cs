66c00b2e08e650cd8dcc9f12a85c2c263ce95b02
Elise T <terranova.design@gmail.com>
2015-12-03 16:14:07 +0100

    no message

---
+++ 4D-Puzzle_Game/Assets/Scripts_v02/FourthDimensionSound.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assets.Scripts_v02 {
+    public class FourthDimensionSound : MonoBehaviour {
+        public List<AudioSource> AudioSources;
+        public float FadeTime;
+
+        private int oldPlayerW;
+
+        void Start() {
+            foreach (AudioSource audio in GetComponentsInChildren<AudioSource>()) {
+                AudioSources.Add(audio);
+                audio.volume = 0;
+                audio.mute = true;
+            }
+            AudioSources[0].mute = false;
+        }
+
+        void Update() {
+            if (oldPlayerW != PickupObjectNew.PlayerW) {
+                StartCoroutine(TweenAudioChange(AudioSources[oldPlayerW], AudioSources[PickupObjectNew.PlayerW], FadeTime));
+            }
+            oldPlayerW = PickupObjectNew.PlayerW;
+        }
+
+        IEnumerator TweenAudioChange(AudioSource current, AudioSource next, float time) {
+            next.mute = false;
+            while (time > 0) {
+                current.volume -= (Time.deltaTime / FadeTime) * 0.5f;
+                next.volume += (Time.deltaTime / FadeTime) * 0.5f;
+                time -= Time.deltaTime;
+                yield return null;
+            }
+            current.mute = true;
+        }
+    }
+}
