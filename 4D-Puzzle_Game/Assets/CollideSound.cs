c137b22355bb5bb9399b59422a87ba3c18a26a37
Alexander Kirk Jørgensen <alkii900@msn.com>
2015-12-05 15:07:56 +0100

    Added Teddy collision sound to 01-EmiL

---
+++ 4D-Puzzle_Game/Assets/CollideSound.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollideSound : MonoBehaviour {
+
+    public AudioSource source;
+
+    void OnCollisionEnter(Collision other) {
+        source.PlayOneShot(source.clip);
+    }
+}
